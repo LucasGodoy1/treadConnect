714b127281a5edf1d0ebcdbf9658920f32329652
Lucas Godoy <lucassdegodoy@hotmail.com>
2026-09-03 11:45:45 -0300

    Feat: desenvolvimento do app iniciado.

---
+++ app/src/main/java/lucasgodoy1/com/github/treadconnect/MainActivity.kt
@@ -0,0 +1,47 @@
+package lucasgodoy1.com.github.treadconnect
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import lucasgodoy1.com.github.treadconnect.ui.theme.TreadConnectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TreadConnectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TreadConnectTheme {
+        Greeting("Android")
+    }
+}
