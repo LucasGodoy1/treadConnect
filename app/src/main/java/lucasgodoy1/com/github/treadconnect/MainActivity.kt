93c7e1cd983cb285b24416879d0a737318b8ff8c
Lucas Godoy <lucassdegodoy@hotmail.com>
2026-09-06 20:52:37 -0300

    feat: implement NavHost and shared BluetoothScanner in MainActivity

--- app/src/main/java/lucasgodoy1/com/github/treadconnect/MainActivity.kt
+++ app/src/main/java/lucasgodoy1/com/github/treadconnect/MainActivity.kt
@@ -1,47 +1,58 @@
 package lucasgodoy1.com.github.treadconnect
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import lucasgodoy1.com.github.treadconnect.bluetoothscanner.BluetoothScanner
+import lucasgodoy1.com.github.treadconnect.ui.view.BuscarBluScreen
 import lucasgodoy1.com.github.treadconnect.ui.theme.TreadConnectTheme
+import lucasgodoy1.com.github.treadconnect.ui.view.ListarDispositivos
+import lucasgodoy1.com.github.treadconnect.ui.view.PainelEsteiraScreen
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TreadConnectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                val context = LocalContext.current
+                val scanner = remember { BluetoothScanner(context) }
+
+                NavHost(navController = navController, startDestination = "buscar") {
+                    composable("buscar") {
+                        BuscarBluScreen(navController, scanner)
+                    }
+                    composable("lista") {
+                        ListarDispositivos(navController, scanner)
+                    }
+                    composable("painel") {
+                        PainelEsteiraScreen(navController, scanner)
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun appPreview() {
     TreadConnectTheme {
-        Greeting("Android")
+        val navController = rememberNavController()
+        val scanner = BluetoothScanner(LocalContext.current)
+        BuscarBluScreen(navController, scanner)
     }
 }
